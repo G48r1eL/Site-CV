4306ceac4472bbff20bf9acf25e348ac6f67c99f
G48r1eL <amarigabriel@gmail.com>
2020-09-16 15:15:23 +0200

    0

--- includes/header.php
+++ includes/header.php
@@ -1,21 +1,21 @@
 <header id="navbar">
-    <div id="logo">LoGo</div>
+    <div id="logo">Gabriel AMARI</div>
     <nav id="nav-top-menu">
                 <ul>
                     <li>
-                        <a>ACCUEIL</a>
+                        <a href="index.php">ACCUEIL</a>
                     </li>
                     <li>
-                        <a>EXPERIENCES</a>
+                        <a href="experiences.php">EXPERIENCES</a>
                     </li>
                     <li>
-                        <a>COMPETENCES</a>
+                        <a href="competences.php">COMPETENCES</a>
                     </li>
                     <li>
-                        <a>FORMATION</a>
+                        <a href="formations.php">FORMATIONS</a>
                     </li>
                     <li>
-                        <a>CONTACT</a>
+                        <a href="contact.php">CONTACT</a>
                     </li>
                 </ul>
     </nav>
@@ -27,9 +27,9 @@
         if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
             document.getElementById("navbar").style.padding = "0px 0px";
             document.getElementById("navbar").style.boxShadow = "0px 2px 1px 0px rgba(0,0,0,0.51)";
-            document.getElementById("logo").style.fontSize = "25px";
+            document.getElementById("logo").style.fontSize = "20px";
         } else {
-            document.getElementById("navbar").style.padding = "10px 0px 10px";
+            document.getElementById("navbar").style.padding = "15px 0px 15px";
             document.getElementById("navbar").style.boxShadow = "0px 0px 0px 0px";
             document.getElementById("logo").style.fontSize = "35px";
         }
--- index.php
+++ index.php
@@ -5,11 +5,26 @@
     <title></title>
     <meta name="description" content="">
     <link href="style/style.css" rel="stylesheet">
+    <script src="https://code.jquery.com/jquery-latest.min.js"></script>
+    <link href="https://fonts.googleapis.com/css2?family=Raleway:wght@200;400;600&display=swap" rel="stylesheet">
 </head>
 
 <body>
     <?php include('includes/header.php');?>
-    <?php include('includes/footer.php');?>
+    <main>
+        <div id="main-prez">
+            <img src="images/photo_G-AMARI.png" id="photo-id"/>
+            <div id="whoami">
+                <h1>Bonjour, je m'appelle Gabriel, j'ai 25 ans et je suis à la recherche d'une alternance en développement informatique pour la rentrée 2020.</h1>
+            </div>
+        </div>
+        <div id="cv">                
+        </div>
+        <div id="aboutme"style="height:1000px;font-size:36px">
+
+        </div>
+    </main>
+    
 </body>
 
 </html>

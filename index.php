98bc9a5e9a0e888f91951635d382e8ca895deec3
G48r1eL <amarigabriel@gmail.com>
2020-09-17 10:15:58 +0200

    a

--- index.php
+++ index.php
@@ -20,11 +20,19 @@
         </div>
         <div id="cv">                
         </div>
-        <div id="aboutme"style="height:1000px;font-size:36px">
-
+        <div id="aboutme">
+            <h1>Présentation</h1>
+            <p>
+                Fasciné par l'informatique depuis de nombreuses années, je souhaite faire de ma passion ma profession.
+                Je recherche actuellement une entreprise dans le domaine du développement informatique pour un contrat en alternance d'une durée de deux ans.
+                J ’ai choisi la voie de l’alternance afin d’être en réel contact avec le monde du travail, ce qui
+                me donnera l’opportunité d’acquérir de l’expérience professionnelle au cours de ma formation, et de
+                m’investir pleinement dans de véritables projets en mettant à votre service toute mon envie
+                d’apprendre. 
+            </p>
         </div>
     </main>
-    
+    <?php include('includes/footer.php');?>
 </body>
 
 </html>
